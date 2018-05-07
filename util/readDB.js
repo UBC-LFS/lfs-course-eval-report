4bd2f8d92320b7a2cb5e87757830f15f0d4b6290
jsondoo <jsondoo@users.noreply.github.com>
2018-05-07 13:56:29 -0700

    Error handling for no database

--- util/readDB.js
+++ util/readDB.js
@@ -5,6 +5,10 @@ const url = 'mongodb://localhost:27017/courseval'
 const readDB = (collectionName) => new Promise((resolve, reject) => {
   MongoClient.connect(url, (err, db) => {
     if (err) reject(err)
+    if(!db) {
+      console.error('No database found. Please ensure that mongo is running.')
+      return;
+    }
     const collection = db.collection(collectionName)
     collection.find({}).toArray((err, result) => {
       if (err) reject(err)
